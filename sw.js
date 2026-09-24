9f4763070f71aae50183137a093b57b93cd2dedc
daguri <daguri75@gmail.com>
2026-09-24 11:27:33 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'best-winner-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/#/',
+  '/manifest.json',
+  '/logo.png',
+  '/og-image.png',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
